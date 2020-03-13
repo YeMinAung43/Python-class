b8584be2594cc689fe4876526974c4699efe556d
YeMinAung <yeminag43@gamil.com>
2020-03-13 11:55:58 +0630

    File Input Output

---
+++ File Input Output/write.py
@@ -0,0 +1,13 @@
+# with open('test.txt', 'r') as rf:
+# 		with open('testCOPY.txt', 'w') as wf:
+
+# 			for line in rf:
+# 				wf.write(line)
+
+with open('cat.jpg', 'r') as rf:
+	with open('cat_copy.jpg', 'w') as wf:
+
+		for line in rf:
+			wf.write(line)
+
+# # >> PIP
